4615a0c4e40b21815f0f976868a8a901a2097484
lucastabosa <42073174+lucastabosa@users.noreply.github.com>
2018-09-12 14:21:55 -0300

    novo php

---
+++ adm.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html> 
+	<head>
+        <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Formulário </title>
+
+
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.min.css" >
+    <!-- Icon -->
+    <link rel="stylesheet" type="text/css" href="assets/fonts/line-icons.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" type="text/css" href="assets/css/slicknav.css">
+    <!-- Nivo Lightbox -->
+    <link rel="stylesheet" type="text/css" href="assets/css/nivo-lightbox.css" >
+    <!-- Animate -->
+    <link rel="stylesheet" type="text/css" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" type="text/css" href="assets/css/responsive.css">
+
+  </head>
+  <body>
+
+    <!-- Header Area wrapper Starts -->
+    <header id="header-wrap">
+      <!-- Navbar Start -->
+      <nav class="navbar navbar-expand-lg fixed-top scrolling-navbar">
+        <div class="container">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <div class="navbar-header">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-navbar" aria-controls="main-navbar" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+              <span class="icon-menu"></span>
+              <span class="icon-menu"></span>
+              <span class="icon-menu"></span>
+            </button>
+            <a href="index.html" class="navbar-brand"><img src="assets/img/brazil.png" alt=""></a>
+          </div>
+          <div class="collapse navbar-collapse" id="main-navbar">
+            <ul class="navbar-nav mr-auto w-100 justify-content-end">
+              <li class="nav-item active">
+                <a class="nav-link" href="#header-wrap">
+                  Cima
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#about">
+                   Brasil
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#schedules">
+                  Seleção
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#team">
+                Jogadores
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#gallery">
+                  Vídeos
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#faq">
+                  Curiosidades
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#sponsors">
+                  Comidas Típicas
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#pricing">
+                  Estádio Maracanã
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#google-map-area">
+                  
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+        </nav>
+      </header>
+    </body>
+</html>
